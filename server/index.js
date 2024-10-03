e40d52d71e4a663f97c1e4e03c272408d6b55c30
Ayush Thakur <zoomgy@Ayushs-MacBook-Air-2.local>
2024-10-04 01:58:02 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import ChatController from './ChatController.js';
 // fGC0iNp8tNS4t7mU
 // mongodb+srv://ayushsinghcs21:fGC0iNp8tNS4t7mU@cluster0.xwjpi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
 const chatBot = new ChatController();
 
 wss.on('connection', function connection(ws) {
